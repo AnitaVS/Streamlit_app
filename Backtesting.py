f243658e69362cace9c0c82e9561f3e85f62776b
AnitaVS <100900078+AnitaVS@users.noreply.github.com>
2022-03-14 12:28:34 +0530

    Update Backtesting.py

--- Backtesting.py
+++ Backtesting.py
@@ -166,7 +166,8 @@ def user_inputs():
             No_of_lots = int(lot_size)
             directory = os.getcwd()
 #             print(directory)
-            path = directory+'\\Test_code\\'
+            path = directory     
+            #+'\\Test_code\\'
             print(path)
             leg_keys = ['instrument', 'action', 'option_type', 'strike_given', 'expiry', 'No_of_lots', 'path']
             leg_values = [instrument, action, option_type, strike_given, expiry, No_of_lots, path]
