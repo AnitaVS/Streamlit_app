44062cb8237292eb21066d78d17e26170eff57a5
AnitaVS <100900078+AnitaVS@users.noreply.github.com>
2022-03-15 12:28:56 +0530

    Changed Title of Line chart

--- Backtesting.py
+++ Backtesting.py
@@ -600,7 +600,7 @@ if __name__ == '__main__':
         print(df)
         st.markdown("Resulted Output:")
         st.dataframe(df)
-        st.write('Total Profit and Loss')
+        st.write('Cumulative Sum of Total Profit and Loss for Backtest period')
         df_PnL = df[['Date','Total PnL']]
         # Convert datetime from datetime64[ns] to string type
         df_PnL['Date'] = df_PnL['Date'].astype(str)
